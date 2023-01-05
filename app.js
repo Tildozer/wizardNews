becb64f818628639965d866a1d297452a851f43b
Nolimitg7 <gsneed24@gmail.com>
2023-01-05 12:07:22 -0500

    can use public folder

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const postBank = require('./postBank')
+app.use(express.static('public'))
 // console.log(postBank.list())
 
 app.get('/',(req,res)=>{
@@ -8,22 +9,36 @@ app.get('/',(req,res)=>{
   const html = `<!DOCTYPE html>
   <html>
   <head>
-  <title>Wizard News</title>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
-  <ul>
-  ${posts.map(post => `<li><p>${post.title}</p><p>${post.name}</p></li>`).join('')}
-  </ul>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
   </body>
-  </html>
-  `
-
-
-
-
-  res.send(html)
-
+</html>`
+  
+res.send(html)
 })
+app.get( '/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  res.send('hello world')
+})
+
 
 const PORT = 1337;
 
