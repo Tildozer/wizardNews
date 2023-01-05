a54a8bb5d8046ddd4455f05913aa18061f6bb74f
Nolimitg7 <gsneed24@gmail.com>
2023-01-05 11:46:30 -0500

    got post to show

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
+const postBank = require('./postBank')
+// console.log(postBank.list())
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/',(req,res)=>{
+  const posts = postBank.list()
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <title>Wizard News</title>
+  </head>
+  <body>
+  <ul>
+  ${posts.map(post => `<li><p>${post.title}</p><p>${post.name}</p></li>`).join('')}
+  </ul>
+  </body>
+  </html>
+  `
+
+
+
+
+  res.send(html)
+
+})
 
 const PORT = 1337;
 
